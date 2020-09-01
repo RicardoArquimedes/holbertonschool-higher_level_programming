483db29762d34a7fa75ba7ced268b9e64bf6c5bb
RicardoArquimedes <richardcqt28@gmail.com>
2020-09-01 17:47:43 +0000

    fix variable

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,9 +8,9 @@ else:
 
 print("Last digit 0f {} is " .format(number), end="")
 
-if m > 5:
-    print("{} and is greater than 5" .format(m))
-if m == 0:
-    print("{} and is 0" .format(m))
 if m < 6 and m != 0:
-    print("{} and is less than 6 and not 0" .format(m))
+    print("{} and is less than 6 and not 0".format(m))
+if m > 5:
+    print("{} and is greater than 5".format(m))
+if m == 0:
+    print("{} and is 0".format(m))
