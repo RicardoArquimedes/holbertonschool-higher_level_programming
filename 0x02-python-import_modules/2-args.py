8314c0e61956539c291caaaa6523c6ad8f035aaa
RicardoArquimedes <richardcqt28@gmail.com>
2020-09-03 18:39:11 +0000

    hot fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,9 +6,9 @@ if len(argv) == 0:
     print(len(argv) - 1, 'arguments.')
 
 elif len(argv) == 1:
-    print("{:d} arguments.".format(len(argv) - 1))
+    print("{:d} arguments:".format(len(argv) - 1))
 
 else:
-    print(len(argv) - 1, 'arguments:')
+    print("{:d} arguments:".format(len(argv) - 1))
     for a in range(1, len(argv)):
         print("{:d}: {:s}".format(a, argv[a]))
