f1de42e9deb7c3b715f777224d7214dc4ecde55b
RicardoArquimedes <richardcqt28@gmail.com>
2020-09-03 18:04:14 +0000

    adding conditional requirment

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,10 @@ b = len(argv) - 1
 if b == 0:
     print(len(argv) - 1, 'arguments.')
 
+elif b == 1:
+    print(len(argv) - 1, 'arguments:')
+    print("{}: {}".format(b, argv[b]))
+
 else:
     print(len(argv) - 1, 'arguments:')
     for a in range(1, len(argv)):
