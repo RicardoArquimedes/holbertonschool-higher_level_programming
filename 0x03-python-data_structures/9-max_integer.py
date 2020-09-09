42a467347803a364eeb6935e5dba60f25d055c3c
RicardoArquimedes <richardcqt28@gmail.com>
2020-09-09 00:53:55 +0000

    adding max_integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) is 0:
+        return None
+    else:
+        max = my_list[0]
+        for m in my_list:
+            if m > max:
+                max = m
+        return max
