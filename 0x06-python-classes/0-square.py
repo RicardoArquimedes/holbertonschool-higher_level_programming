f57be6470f6091ea35eab1ef8afa394413a6c229
RicardoArquimedes <richardcqt28@gmail.com>
2020-09-23 02:35:12 +0000

    adding comments to print empty square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
+"""0-square.py
+This module contains an empty class
+"""
+
 
 class Square:
+    """
+    This is an empty class Square that defines a square
+    """
     pass
