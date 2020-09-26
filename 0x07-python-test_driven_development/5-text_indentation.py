07aa7288493c1c57704eaa2df2cca87c95e2686f
RicardoArquimedes <richardcqt28@gmail.com>
2020-09-26 14:56:48 +0000

    updating text_indentantion

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,14 +6,16 @@ each of these characters: ., ? and :
 
 
 def text_indentation(text):
-    """Print the square with the caracter #"""
+    """
+    Arguments:
+    text: letter in text
+    """
     if type(text) is not str:
         raise TypeError("text must be a string")
-    a = 0
-    while a < len(text):
-        print("{}".format(text[a]), end="")
-        if text[a] in [".", "?", ":"]:
-            print("")
-            print("")
-            a = a + 1
-        a = a + 1
+    tmp = 0
+    for i, c in enumerate(text):
+        if c in ".?:":
+            print(text[tmp:i + 1].strip(), end="\n\n")
+            tmp = i + 1
+        elif i == len(text) - 1:
+            print(text[tmp:i + 1].strip(), end="")
