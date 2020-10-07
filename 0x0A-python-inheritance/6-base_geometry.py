5e52b667f24f3cf17f475336dfba64f29e2429e2
RicardoArquimedes <richardcqt28@gmail.com>
2020-10-07 00:28:50 +0000

    add tasks

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module Write a class BaseGeometry
+(based on 5-base_geometry.py).
+Raises:
+    Exception: [description]
+"""
+
+
+class BaseGeometry():
+        """Class
+        """
+
+        def area(self):
+                """Method
+                """
+                raise Exception("area() is not implemented")
