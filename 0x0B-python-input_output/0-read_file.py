a1f5756cb90204d0f92a753d605b63b7e43dec1d
RicardoArquimedes <richardcqt28@gmail.com>
2020-10-07 21:58:41 +0000

    fixxing end

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Read a text file (UTF-8)"""
 
-
 def read_file(filename=""):
     """Function to read a text file (UTF8)"""
-    with open(filename, mode='r') as my_file:
-        print(my_file.read())
+    with open(filename, mode='r', encoding="utf-8") as my_file:
+        print(my_file.read(), end="")
