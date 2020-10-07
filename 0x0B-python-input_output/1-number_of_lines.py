509a67d45c86051aa24e3a5dc5f68b0c8bfc55ee
RicardoArquimedes <richardcqt28@gmail.com>
2020-10-07 22:04:30 +0000

    fixxing number

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
-"""In this module it create a function that read
-    n lines of a file
-"""
+"""Return numbers of lines"""
 
 
-def read_lines(filename="", nb_lines=0):
-    """This  function that reads n lines of a text file
-    (UTF8) and prints it to stdout:
-    Arguments:
-        filename {str} -- This is the file to read(default: {""})
-        nb_lines {int} -- This is the number line to read(default: {0})
-    """
-    with open(filename, mode='r', encoding='utf-8') as my_file:
-        f_list = my_file.readlines()
-        if nb_lines <= 0 or nb_lines > len(f_list):
-            nb_lines = len(f_list)
-        for i in range(nb_lines):
-            print(f_list[i], end='')
-    return i + 1
+def number_of_lines(filename=""):
+        """Function that read a file and count number of lines.
+        Args:
+            filename (str, optional): file path. Defaults to "".
+        """
+        with open(filename, mode='r', encoding="utf-8") as my_file:
+            for i, l in enumerate(my_file):
+                pass
+        return i
