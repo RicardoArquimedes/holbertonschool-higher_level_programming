86830aec1768294258ad75937f418f763ffa81ad
RicardoArquimedes <richardcqt28@gmail.com>
2020-10-07 22:32:18 +0000

    fixxing pep8

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -7,6 +7,6 @@ def write_file(filename="", text=""):
     """Keyword Arguments:
         filename {str} -- the file to read(default: {""})
         text {str} -- Text to create in a new file (default: {""})
-    """   
+    """
     with open(filename, mode='w', encoding='utf-8') as my_file:
         return my_file.write(text)
