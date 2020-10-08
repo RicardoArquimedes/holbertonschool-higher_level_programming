1f3ab2838cc96c5175db430e61f296440cf87dda
RicardoArquimedes <richardcqt28@gmail.com>
2020-10-08 01:46:24 +0000

    adding save_to_json

---
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module that coverte a object-python to str-JSON in a
+   jsonfile
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+        """Function that save to json file
+        Args:
+            my_obj ([type]): object-python
+            filename ([type]): name of jsonfile
+        """
+        with open(filename, mode='w', encoding='utf-8') as my_fileJSON:
+                json.dump(my_obj, my_fileJSON)
