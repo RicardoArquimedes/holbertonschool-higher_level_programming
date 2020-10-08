f38a750289fe09c91e9b151d6f86adda09665de7
RicardoArquimedes <richardcqt28@gmail.com>
2020-10-08 02:34:03 +0000

    updating save jsno

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -13,5 +13,5 @@ def save_to_json_file(my_obj, filename):
             my_obj ([type]): object-python
             filename ([type]): name of jsonfile
         """
-        with open(filename, mode='w', encoding='utf-8') as my_fileJSON:
+        with open(filename, mode='w') as my_fileJSON:
                 json.dump(my_obj, my_fileJSON)
