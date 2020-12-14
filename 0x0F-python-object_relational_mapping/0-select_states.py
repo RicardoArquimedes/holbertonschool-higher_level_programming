14a668cb107560411e5987fc065a960b6f955e3c
RicardoArquimedes <richardcqt28@gmail.com>
2020-12-13 22:13:24 -0500

    adding task 10

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-"""Exercise
-"""
+"""Module doc"""
+
+
 import MySQLdb
 from sys import argv
 
+if __name__ == "__main__":
+    user_db = argv[1]
+    pass_db = argv[2]
+    name_db = argv[3]
 
-if __name__ == '__main__':
-    """COnect
-    """
-    db = MySQLdb.connect('localhost', argv[1], argv[2], argv[3], port=3306)
-    cur = db.cursor()
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=user_db,
+                         passwd=pass_db,
+                         db=name_db,
+                         charset="utf8")
 
-    cur.execute('SELECT * FROM states ORDER BY id ASC')
-    for row in cur.fetchall():
-        print(row)
+    cursor = db.cursor()
 
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    data = cursor.fetchall()
+
+    for states in data:
+        print(states)
+
+    cursor.close()
     db.close()
