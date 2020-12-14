a4e267cd77a8ebdd3f9281d2742a8bddb07d4c9c
RicardoArquimedes <richardcqt28@gmail.com>
2020-12-13 23:22:46 -0500

    adding delete state and fetching city by state

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Start link class to table in database"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import select
+
+
+if __name__ == "__main__":
+
+    connect = 'mysql+mysqldb://{}:{}@localhost:3306/{}'. \
+        format(argv[1], argv[2], argv[3])
+
+    engine = create_engine(connect, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State).filter(State.name.like('%a%')).all()
+
+    for row in query:
+        session.delete(row)
+
+    session.commit()
+    session.close()
