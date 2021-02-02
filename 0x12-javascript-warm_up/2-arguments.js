8fbc5d9b2d1971eef41068170e9b0ed3cb36b94f
RicardoArquimedes <richardcqt28@gmail.com>
2021-02-02 12:39:27 -0500

    fixxing camel case

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // script that prints a message depending of the number o= arguments passed
-const args_passed = process.argv.length;
-if (args_passed === 2) {
+const ArgsPassed = process.argv.length;
+if (ArgsPassed === 2) {
   console.log('No argument');
-} else if (args_passed === 3)  {
+} else if (ArgsPassed === 3)  {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
