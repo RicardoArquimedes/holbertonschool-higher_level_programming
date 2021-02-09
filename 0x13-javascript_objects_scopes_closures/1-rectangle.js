139ac8a6737cb920ec2d417cbefb756f07951fdb
RicardoArquimedes <richardcqt28@gmail.com>
2021-02-08 19:53:52 -0500

    fixxing variable names

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor (height, width) {
-    this.height = height;
-    this.width = width;
+  constructor (h, width) {
+    this.height = h;
+    this.width = w;
   }
 }
 module.exports = Rectangle;
