b6dcc34032f5c5cab0b5ecfa63a10806a139055d
RicardoArquimedes <richardcqt28@gmail.com>
2021-02-09 20:12:51 -0500

    adding task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(
+  {
+    url: url,
+    json: true
+  },
+  (err, response, body) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (response.statusCode === 200) {
+        const store = {};
+        body.forEach(element => {
+          const { userId, completed } = element;
+          if (completed) {
+            if (store[userId]) {
+              store[userId]++;
+            } else {
+              store[userId] = 1;
+            }
+          }
+        });
+        console.log(store);
+      }
+    }
+  }
+  
